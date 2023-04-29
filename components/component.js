b64b36ecfd4ec85734c10c7892d3cf032c37d4bf
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-28 18:44:16 -0700

    updated file

--- components/component.js
+++ components/component.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import styles from '../styles/style.module.css';
+import React from "react";
+import styles from "../styles/style.module.css";
 
 const Component = () => {
   return (
-    <div className={styles.container}>
+    <div style={{backgroundColor: "black", minHeight: "100vh"}}>
       <nav className={styles.navbar}>
-        <a href="#" className={styles.navLink}>About</a>
-        <a href="#" className={styles.navLink}>Projects</a>
-        <a href="#" className={styles.navLink}>Sign In</a>
+        <div className={styles.navLeft}>
+          <h1>Test Landing</h1>
+        </div>
+        <div className={styles.navRight}>
+          <a href="#">About</a>
+          <a href="#">Tests</a>
+          <a href="#">Start</a>
+        </div>
       </nav>
-      <main>
-        <h1 className={styles.heroTitle}>Basic Landing</h1>
-        <p className={styles.paragraph}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu metus a massa ultrices ullamcorper vel id elit. Praesent bibendum purus eget dapibus iaculis.
-        </p>
-      </main>
+      <div className={styles.hero}>
+        <h2>Test Landing</h2>
+      </div>
+      <div className={styles.paragraph}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu metus a massa ultrices ullamcorper vel id elit. Praesent bibendum purus eget dapibus iaculis. </p>
+      </div>
       <footer className={styles.footer}>
-        <p>Copyright  2021
-        </p>
+        <p>Copyright &copy; 2023 Test Landing</p>
       </footer>
     </div>
   );
